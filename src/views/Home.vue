73fbc623ea0921db8f363a0034679e53c6bab7f0
mztkn <lijiachen633@gmail.com>
2020-12-20 18:46:22 +0800

    去掉dialog

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,61 +19,78 @@
           <span style="fontWeight: 700">{{item.name}}</span>
         </div>
         <div
-          class="index"
+          class="out"
           v-for="(i,n) in item.children"
           :key="i.name"
         >
-          <div
-            class="arrow"
-            v-if="isToggle"
-          >
-            <ArrowUpOutlined
-              style="marginRight: 5px"
-              @click="toggleIndex(item.children, n, 'on')"
-              v-if="n !== 0 "
-            />
-            <StopOutlined
-              v-else
-              style="marginRight: 5px;color:#c73a3a"
-            />
-            <ArrowDownOutlined
-              @click="toggleIndex(item.children, n, 'down')"
-              v-if="n !== item.children.length - 1 "
-            />
-            <StopOutlined
-              v-else
-              style="color:#c73a3a"
-            />
-          </div>
-          <div>
-            <i></i>
-            <span>{{i.name}}</span>
-          </div>
-          <div style="marginRight:10px">
-            <PieChartOutlined style="marginRight: 5px" />
-            <span v-if="!i.disbled">{{i.schedule}}%</span>
-            <a-input-number
-              v-else
-              :min="0"
-              :max="100"
-              size="small"
-              v-model:value="i.schedule"
-              @blur="i.disbled = false"
-            ></a-input-number>
-            <a-popconfirm
-              title="确定删除此指标？"
-              ok-text="确定"
-              cancel-text="取消"
-              @confirm="delIndex(item.children,n)"
+          <div class="index">
+            <div
+              class="arrow"
+              v-if="isToggle"
             >
-              <MinusCircleOutlined style="color:#e2333c;marginLeft: 10px" />
-            </a-popconfirm>
+              <ArrowUpOutlined
+                style="marginRight: 5px"
+                @click="toggleIndex(item.children, n, 'on')"
+                v-if="n !== 0 "
+              />
+              <StopOutlined
+                v-else
+                style="marginRight: 5px;color:#c73a3a"
+              />
+              <ArrowDownOutlined
+                @click="toggleIndex(item.children, n, 'down')"
+                v-if="n !== item.children.length - 1 "
+              />
+              <StopOutlined
+                v-else
+                style="color:#c73a3a"
+              />
+            </div>
+            <div>
+              <i></i>
+              <span>{{i.name}}</span>
+            </div>
+            <div style="marginRight:10px">
+              <PieChartOutlined style="marginRight: 5px" />
+              <span v-if="!i.disbled">{{i.schedule}}%</span>
+              <a-input-number
+                v-else
+                :min="0"
+                :max="100"
+                size="small"
+                v-model:value="i.schedule"
+                @blur="i.disbled = false"
+              ></a-input-number>
+              <a-popconfirm
+                title="确定删除此指标？"
+                ok-text="确定"
+                cancel-text="取消"
+                @confirm="delIndex(item.children,n)"
+              >
+                <MinusCircleOutlined
+                  style="color:#e2333c;marginLeft: 10px;"
+                  class="del"
+                />
+              </a-popconfirm>
+            </div>
           </div>
+        </div>
+        <div
+          class="put"
+          v-if="isAdd && index === idx"
+        >
+          <a-input
+            id="ix"
+            v-model:value="targerVal"
+            placeholder="请输入指标内容，按回车确认"
+            @pressEnter="addIndex(item)"
+            @blur="addIndex(item)"
+          ></a-input>
         </div>
         <div class="down">
           <a-button
             size="small"
-            @click="isAdd = true; title = '新增指标'; isCreate = true; index = idx"
+            @click="isAdd = true; index = idx;focusOn('b')"
           >
             <template #icon>
               <PlusSquareOutlined />
@@ -120,35 +137,31 @@
           </a-popconfirm>
         </div>
       </div>
+      <div
+        style="marginBottom: 20px;width: 40%"
+        v-if="isCreate"
+      >
+        <a-input
+          placeholder="请输入目标名称,按回车确认"
+          id="ip"
+          v-model:value="title"
+          @blur="addIndex"
+          @pressEnter="addIndex"
+        ></a-input>
+      </div>
     </main>
     <!-- 底 -->
     <footer>
-      <div @click="isCreate = true; title = '新增目标'; isAdd = false">
+      <div @click="focusOn('a')">
         <PlusOutlined style="marginRight:5px" />
         <span>新增目标</span>
       </div>
     </footer>
-    <!-- modal框 -->
-    <a-modal
-      v-model:visible="isCreate"
-      :title="title"
-      :maskClosable="false"
-      ok-text="确认"
-      cancel-text="取消"
-      @cancel="isCreate = false"
-      @ok="confirm"
-    >
-      <a-input
-        v-model:value="targerVal"
-        :placeholder="isAdd ? '请输入指标名称': '请输入目标名称'"
-      ></a-input>
-      <!-- <a-input placeholder="进度"></a-input> -->
-    </a-modal>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref, watch } from 'vue'
+import { defineComponent, nextTick, reactive, ref } from 'vue'
 import {
   AimOutlined,
   PieChartOutlined,
@@ -196,8 +209,28 @@ export default defineComponent({
     ])
     const dblclick = (e: any) => {
       e.target.setAttribute('contenteditable', 'true')
-      console.log(e.target)
     }
+    const targerVal = ref<string>('')
+    const isAdd = ref<boolean>(false)
+    const title = ref<string>('')
+    const isCreate = ref<boolean>(false)
+    const focusOn = (z: string) => {
+      if (z === 'a') {
+        isCreate.value = true
+        nextTick(() => {
+          const dom: any = document.getElementById('ip')
+          dom.focus()
+        })
+      }
+      if (z === 'b') {
+        isAdd.value = true
+        nextTick(() => {
+          const dom: any = document.getElementById('ix')
+          dom.focus()
+        })
+      }
+    }
+
     // 指标部分
     const isToggle = ref<boolean>(false)
     const delTar = (i: number) => {
@@ -207,6 +240,36 @@ export default defineComponent({
     const delIndex = (v: [], i: number) => {
       v.splice(i, 1)
       message.success('删除成功', 1)
+    }
+    const addIndex = (item: List = { name: '', children: [] }) => {
+      if (isAdd.value) {
+        targerVal.value &&
+          item.children.push({
+            name: targerVal.value,
+            schedule: 0,
+            disbled: false
+          })
+        targerVal.value ||
+          item.children.push({
+            name: '新指标',
+            schedule: 0,
+            disbled: false
+          })
+        isAdd.value = false
+      }
+      if (isCreate.value) {
+        title.value &&
+          target_list.value.push({
+            name: title.value,
+            children: []
+          })
+        title.value ||
+          target_list.value.push({
+            name: '新目标',
+            children: []
+          })
+        isCreate.value = false
+      }
     }
     const toggleIndex = (val: [], i: number, order: string) => {
       order === 'on' && ([val[i], val[i - 1]] = [val[i - 1], val[i]])
@@ -218,43 +281,12 @@ export default defineComponent({
       })
     }
     const index = ref<number>(0)
-    // 遮罩部分
-    const isCreate = ref<boolean>(false)
-    const isAdd = ref<boolean>(false)
-    const title = ref<string>('')
-    const targerVal = ref<string>('')
-    const confirm = () => {
-      if (!targerVal.value) {
-        return message.error('请输入名称', 1)
-      }
-      // 添加指标
-      isAdd.value &&
-        target_list.value[index.value].children.push({
-          name: targerVal.value,
-          schedule: 0,
-          disbled: false
-        })
-      // 添加目标
-      isAdd.value ||
-        target_list.value.push({ name: targerVal.value, children: [] })
-      isCreate.value = false
-    }
-    watch(
-      [isCreate],
-      ([val]) => {
-        if (!val) {
-          title.value = ''
-          targerVal.value = ''
-        }
-      },
-      { deep: true }
-    )
+
     return {
       target_list,
       isCreate,
       title,
       targerVal,
-      confirm,
       delTar,
       isAdd,
       index,
@@ -262,7 +294,9 @@ export default defineComponent({
       delIndex,
       toggleIndex,
       isToggle,
-      dblclick
+      dblclick,
+      addIndex,
+      focusOn
     }
   }
 })
@@ -293,30 +327,36 @@ export default defineComponent({
           color #1890ff
           margin-left 20px
           margin-right 10px
-      .index
-        display flex
-        justify-content space-between
-        align-items center
-        position relative
-        height 30px
-        margin 0 20px 0 50px
-        border-bottom 1px solid rgba(0, 0, 0, 0.32)
-        .arrow
-          position absolute
-          left -45px
+      .put
+        margin 10px 20px 0 50px
+      .out
+        .index
+          display flex
+          justify-content space-between
+          align-items center
+          position relative
+          cursor pointer
           height 30px
-          line-height 30px
-          span
-            cursor pointer
-        &:hover
-          background #f5f7fa
-        i
-          display inline-block
-          width 8px
-          height 8px
-          border-radius 10px
-          background #1890ff
-          margin-right 10px
+          margin 0 20px 0 50px
+          border-bottom 1px solid rgba(0, 0, 0, 0.32)
+          .arrow
+            position absolute
+            left -45px
+            height 30px
+            line-height 30px
+          i
+            display inline-block
+            width 8px
+            height 8px
+            border-radius 10px
+            background #1890ff
+            margin-right 10px
+          .del
+            visibility hidden
+          &:hover
+            background #f5f5fb
+            .del
+              visibility visible
       .down
         height 40px
         line-height 40px
